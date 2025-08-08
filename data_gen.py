2604ab2852591771503727ff182a7d79d7facb73
Ahmet Can Çömez <cancmz23@gmail.com>
2025-08-08 18:20:20 +0300

    Generating random circles and drawing

--- data_gen.py
+++ data_gen.py
@@ -7,17 +7,30 @@ class Circle:
         self.center_y = rn.uniform(-50, 50)
         self.radius = rn.uniform(50, 100)
 
+colours=['blue','red','orange','black']
 circles = [Circle() for _ in range(3)]
-
 fig, ax = plt.subplots()
+legend_labels=[]
 
-for c in circles:
-    circle_patch = plt.Circle((c.center_x, c.center_y), c.radius,fill=False, edgecolor='blue', linewidth=2)
+for i, c in enumerate(circles, start=1):
+    circle_colour=rn.choice(colours)
+    circle_patch = plt.Circle((c.center_x, c.center_y), c.radius,fill=False, edgecolor=circle_colour, linewidth=1)
+
     ax.add_patch(circle_patch)
-    ax.plot(c.center_x,c.center_y,'ro')
+    ax.plot(c.center_x,c.center_y,'ro',markersize=4)
+    ax.text(c.center_x+3,c.center_y+3,  f"({c.center_x:.1f}, {c.center_y:.1f})", fontsize=8, color='black')
+
+    legend_labels.append((f"Circle {i} - R={c.radius:.1f}", circle_colour))
+
+for label, color in legend_labels:
+    ax.plot([], [], color=color, label=label)
+
+ax.axhline(0, color='black', linewidth=1) 
+ax.axvline(0, color='black', linewidth=1)
 
 ax.set_xlim(-200, 200)
 ax.set_ylim(-200, 200)
 ax.set_aspect('equal', adjustable='box')
 plt.grid(True)
+plt.legend(loc='upper right')
 plt.show()
