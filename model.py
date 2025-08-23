c4d5ddcd6572cbf46f79c8210eadd1a4b01d853d
Ahmet Can Çömez <cancmz23@gmail.com>
2025-08-24 00:55:04 +0300

    Implement RANSAC-based single circle detection
    - Added count_inliers to evaluate circle models
    - Implemented RANSAC loop with 5500 iterations
    - Selected best circle model based on maximum inliers
    - Added visualization of inliers/outliers and fitted circle

--- model.py
+++ model.py
@@ -33,7 +33,59 @@ def choose_and_fit():
     sample = df.sample(n=3)[["x", "y"]].to_numpy()
     p1, p2, p3 = sample[0], sample[1], sample[2]
     result = fit_circle_3pts(p1, p2, p3)
-    return (p1, p2, p3), result
+    if result is None:
+        return (p1, p2, p3), None
+    else:
+        return (p1, p2, p3), result
 
-(points, circle) = choose_and_fit()
-p1, p2, p3 = points
+def count_inliers(points,cx,cy,r, threshold=5.0):
+    inliers = []
+    for (x,y) in points:
+        d=math.hypot(x-cx,y-cy)
+        error=abs(d-r)
+        if error < threshold:
+            inliers.append((x,y))
+    return inliers
+
+df=pd.read_csv("points.csv")
+points=df[["x","y"]].to_numpy()
+
+n_iter=5500
+best_model = None
+best_inliers = []
+number_of_inliers = 0
+for _ in range(n_iter):
+    (p1, p2, p3), result = choose_and_fit()
+    if result is None:
+        continue
+    cx, cy, r = result
+    inliers = count_inliers(points, cx, cy, r)
+    if len(inliers) > number_of_inliers:
+        number_of_inliers = len(inliers)
+        best_inliers = inliers
+        best_model = cx, cy, r
+
+print("Best circle:", best_model)
+print("Number of inlier:", len(best_inliers), "/", len(points))
+
+fig, ax = plt.subplots()
+for (x,y) in points:
+    if (x,y) in best_inliers:
+        ax.plot(x, y, "bo", markersize=4)  # inlier
+    else:
+        ax.plot(x, y, "ro", markersize=4)  # outlier
+
+"""ax.plot(p1[0], p1[1],"ro", markersize=4)
+ax.plot(p2[0], p2[1],"ro", markersize=4)
+ax.plot(p3[0], p3[1],"ro", markersize=4)
+
+circle_patch = Circle((cx, cy), r, fill=False, color="black", linewidth=2)
+ax.add_patch(circle_patch)
+ax.plot(cx, cy, "kx", markersize=6)"""
+
+ax.axhline(0, color="black", linewidth=1)
+ax.axvline(0, color="black", linewidth=1)
+ax.set_xlim(-200, 200)
+ax.set_ylim(-200, 200)
+ax.set_aspect("equal", adjustable="box")
+plt.show()
